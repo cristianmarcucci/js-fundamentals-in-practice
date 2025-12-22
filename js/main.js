8ac97fa496f2ff6a257faddc7a41b06c2ba855b2
unknown <cristian.marcucci@gmail.com>
2025-12-22 16:34:37 -0300

    feat: create tasks array and addTask function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("JS Fundamentals project started")
+const tasks = [];
+
+function addTask(title) {
+    const task = {
+        id: Date.now(),
+        title: title,
+        complete: false,
+    }
+
+    tasks.push(task)
+}
+
+addTask("Study JavaScript");
+addTask("Practice Git");
+addTask("Build projects");
+
+console.log(tasks);
